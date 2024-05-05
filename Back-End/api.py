5ca13713c085eaff100a370dc05499de5101479b
Gustavo Costa <gustavo.costa@sou.inteli.edu.br>
2024-05-05 14:17:11 -0300

    fix: corrections on search for the reports

--- Back-End/api.py
+++ Back-End/api.py
@@ -26,15 +26,24 @@ async def get_historico_pdf(data: MyData):
 
     id_detento = data.id_detento
     historico = data.historico
+    
+    folder_path = f"./Reports/{id_detento}/"
+    pdf_file = ""
 
-    ultimo_comentario = historico[-1]
-    comportamento = ultimo_comentario["comportamento"]
-    data = str(ultimo_comentario["data"]).replace("/", "-")
+    for item in os.listdir(folder_path):
+        if item.lower().endswith('.pdf'):
+            pdf_file = item
 
-    file_name = f"DetaineeReport_{id_detento}_{comportamento}_{data}.pdf"
-    await convert_to_pdf(detainee_report, file_name)
+    return FileResponse("./Reports/" + id_detento + "/" + pdf_file)
 
-    os.rename("./" + file_name, "./Reports/" + file_name)
+    # ultimo_comentario = historico[-1]
+    # comportamento = ultimo_comentario["comportamento"]
+    # data = str(ultimo_comentario["data"]).replace("/", "-")
 
-    file_path = f"./Reports/{file_name}"
-    return FileResponse(file_path)
+    # file_name = f"DetaineeReport_{id_detento}_{comportamento}_{data}.pdf"
+    # await convert_to_pdf(detainee_report, file_name)
+
+    # os.rename("./" + file_name, "./Reports/" + file_name)
+
+    # file_path = f"./Reports/{file_name}"
+    # return FileResponse(file_path)
