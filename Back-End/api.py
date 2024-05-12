2ea7ce9c0419fda7a3c0e1282e2f708519577461
Gustavo Costa <gustavo.costa@sou.inteli.edu.br>
2024-05-11 21:39:43 -0300

    chore: create docker image

--- Back-End/api.py
+++ Back-End/api.py
@@ -38,23 +38,23 @@ async def get_historico_pdf(data: MyData):
     id_detento = data.id_detento
     historico = data.historico
     
-    folder_path = f"./Reports/{id_detento}/"
-    pdf_file = ""
+    # folder_path = f"./Reports/{id_detento}/"
+    # pdf_file = ""
 
-    for item in os.listdir(folder_path):
-        if item.lower().endswith('.pdf'):
-            pdf_file = item
+    # for item in os.listdir(folder_path):
+    #     if item.lower().endswith('.pdf'):
+    #         pdf_file = item
 
-    return FileResponse("./Reports/" + id_detento + "/" + pdf_file)
+    # return FileResponse("./Reports/" + id_detento + "/" + pdf_file)
 
-    # ultimo_comentario = historico[-1]
-    # comportamento = ultimo_comentario["comportamento"]
-    # data = str(ultimo_comentario["data"]).replace("/", "-")
+    ultimo_comentario = historico[-1]
+    comportamento = ultimo_comentario["comportamento"]
+    data = str(ultimo_comentario["data"]).replace("/", "-")
 
-    # file_name = f"DetaineeReport_{id_detento}_{comportamento}_{data}.pdf"
-    # await convert_to_pdf(detainee_report, file_name)
+    file_name = f"DetaineeReport_{id_detento}_{comportamento}_{data}.pdf"
+    await convert_to_pdf(detainee_report, file_name)
 
-    # os.rename("./" + file_name, "./Reports/" + file_name)
+    os.rename("./" + file_name, "./Reports/" + file_name)
 
-    # file_path = f"./Reports/{file_name}"
-    # return FileResponse(file_path)
+    file_path = f"./Reports/{file_name}"
+    return FileResponse(file_path)
